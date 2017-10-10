20d7dbd87493b9ac2f9d851f1c3be461f4c0fcb3
root <root@localhost.localdomain>
2017-10-10 12:11:01 +0200

    FINAL HAHAHA

--- launchDocker.sh
+++ launchDocker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ipAddress=$(echo `ifconfig wlp3s0 2>/dev/null|awk '/inet / {print $2}'|sed 's/addr://'`)
 
+if [ -z "$ipAddress" ]; then
+        ipAddress=$(echo `ifconfig enp0s3 2>/dev/null|awk '/inet / {print $2}'|sed 's/addr://'`)
+fi
+
 user=$1
 pass=$2
 mail=$3
