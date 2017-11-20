dc7d08344c5a81f9b3584c6a205594163c8e1eda
sysadmin <sysadmin@sphere.home>
2017-11-20 21:30:16 +0100

    rename docker

---
+++ git.sh
@@ -0,0 +1,7 @@
+#/!bin/bash
+
+git init
+
+git remote add repo https://github.com/Miloodzy/$1
+
+git pull repo master
--- launchDocker.sh
+++ launchDocker.sh
@@ -2,7 +2,7 @@
 ipAddress=$(echo `ifconfig wlp3s0 2>/dev/null|awk '/inet / {print $2}'|sed 's/addr://'`)
 
 if [ -z "$ipAddress" ]; then
-        ipAddress=$(echo `ifconfig enp0s3 2>/dev/null|awk '/inet / {print $2}'|sed 's/addr://'`)
+        ipAddress=$(echo `ifconfig enp4s0 2>/dev/null|awk '/inet / {print $2}'|sed 's/addr://'`)
 fi
 echo $ipAddress
 
